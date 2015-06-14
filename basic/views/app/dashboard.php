b2c78e13f6bff01a7cb0a434e978e08121535297
denniseik <denniseik@bitbucket.org>
2015-06-14 16:07:24 +0200

    dashboard analytics

--- basic/views/app/dashboard.php
+++ basic/views/app/dashboard.php
@@ -1,1 +1,58 @@
 <h1>Dashboard</h1>
+<div class="row">
+
+    <div class="col-md-9">
+
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Analytics
+            </div>
+            <div class="panel-body">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="thumbnail">
+                            <div class="caption">
+                                Computer use time graph
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="thumbnail">
+                            <div class="caption">
+
+                                graph here
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Installation help
+            </div>
+            <div class="panel-body">
+                ??? Description of how the device works and how to install ???
+            </div>
+        </div>
+
+    </div>
+
+    <div class="col-md-3">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Summary
+            </div>
+            <div class="panel-body">
+                <table class="table table-striped">
+                    <tr><td>Last action:</td><td>17:53</td></tr>
+                    <tr><td>Device:</td><td><span class="label label-success">Online</span></td></tr>
+                    <tr><td>Filters active:</td><td>2</td></tr>
+                    <tr><td>Mood: </td><td>normal</td></tr>
+                </table>
+            </div>
+        </div>
+    </div>
+
+</div>
